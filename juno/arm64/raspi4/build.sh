54e83efc81b1feb45848e580e7928fb91d01aed2
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2019-11-06 18:25:53 +0100

    Update base image

--- juno/arm64/raspi4/build.sh
+++ juno/arm64/raspi4/build.sh
@@ -8,7 +8,7 @@ YYYYMMDD="$(date +%Y%m%d)"
 OUTPUT_SUFFIX=".img"
 TARGET_IMG="elementaryos-${VERSION}-${TARGET}.${YYYYMMDD}${OUTPUT_SUFFIX}"
 
-BASE_IMG_URL="https://github.com/TheRemote/Ubuntu-Server-raspi4-unofficial/releases/download/v17/ubuntu-18.04.3-preinstalled-server-arm64+raspi4.img.xz"
+BASE_IMG_URL="https://github.com/TheRemote/Ubuntu-Server-raspi4-unofficial/releases/download/v19/ubuntu-18.04.3-preinstalled-server-arm64+raspi4.img.xz"
 BASE_IMG="ubuntu-18.04.3-preinstalled-server-arm64+raspi4.img"
 MountXZ=""
 
